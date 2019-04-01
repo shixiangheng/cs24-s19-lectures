a38fe11bfbb007ed2138f970671c6368feaddfd7
Diba Mirza <dimirza@cs.ucsb.edu>
2019-04-01 12:42:28 -0700

    lec 01

--- lect-01/simpleProgram.cpp
+++ lect-01/simpleProgram.cpp
@@ -3,7 +3,30 @@
 
 using namespace std;
 
+class Date{
+public:
+	void setDate(int mm , int dd);
+	void input();
+	int getMonth();
+	int getDay();
+	//member variables
+	int month;
+	int day;
+};
+
+
 int main(){
-	cout<<"Hello world!"<<endl;
+	Date today;
+	Date birthday;
+	today.setDate(4, 1);
+	cout<<"Enter your birthday: ";
+	birthday.input();
+	//cin>>birthday.month>>birthday.day;
+	if(today.getMonth() == birthday.getMonth() 
+		&& today.getDay() == birthday.getDay())
+		cout<<"Happy Birthday!"<<endl;
+	else
+		cout<<"Oh well!"<<endl;
+
 	return 0;
 }
